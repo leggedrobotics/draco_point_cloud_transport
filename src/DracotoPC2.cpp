e0dba02d53d0c5fdcf4a4c654638d13bf0c1ccab
Marco Tranzatto <marcot@ethz.ch>
2020-10-01 16:08:40 +0200

    Update DracotoPC2.cpp

--- src/DracotoPC2.cpp
+++ src/DracotoPC2.cpp
@@ -1,5 +1,5 @@
 #include "draco_point_cloud_transport/DracotoPC2.h"
-#include "debug_msg.h"
+#include "point_cloud_transport/debug_msg.h"
 
 //! Constructor
 DracotoPC2::DracotoPC2(std::unique_ptr<draco::PointCloud> && pc, const draco_point_cloud_transport::CompressedPointCloud2ConstPtr & compressed_PC2)
